40bcbfcbce0dfc94d748b47fae45357eab3570e0
Demarillac Izere <izered3@gmail.com>
2020-12-04 06:40:50 +0200

    adding a navigation bar

---
+++ app/main/views.py
@@ -0,0 +1,11 @@
+from . import main
+from flask_login import login_required, current_user
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User, Comment, Pitch, Like, Dislike
+# from .forms import CommentForm, PitchForm, UpdateProfile
+from .. import db, photos
+
+@main.route('/')
+def index():
+    title="Shout out"
+    return render_template('index.html', title = title)
