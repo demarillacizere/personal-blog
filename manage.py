5954e07ddc250da3e43c4580cef016c386fe03ac
Demarillac Izere <izered3@gmail.com>
2020-12-04 20:04:16 +0200

    updated the authentication system

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 from app import create_app,db
+from app.models import User
 from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
 
@@ -20,6 +21,6 @@ def test():
     
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db )
+    return dict(app = app,db = db, User=User )
 if __name__ == '__main__':
     manager.run()
