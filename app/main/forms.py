e6630bc78da8f40ecfec4e1ee13be1733d16b944
Demarillac Izere <izered3@gmail.com>
2020-12-04 07:58:57 +0200

    updated the views

---
+++ app/main/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+class BlogForm(FlaskForm):
+
+ title = StringField('blog title',validators=[Required()])
+ content = TextAreaField('blog', validators=[Required()])
+ category = SelectField('Category', choices=[('Interview','Interview'),('Advertisement','Advertisement'),('Project','Project'),('General','General'),('Pickuplines','Pickuplines')], validators=[Required()])
+ submit = SubmitField('Submit')
+ 
+
+class CommentForm(FlaskForm):
+
+ comment = TextAreaField('Comment', validators=[Required()])
+
+ submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from . import main
 from flask_login import login_required, current_user
 from flask import render_template,request,redirect,url_for,abort
-# from ..models import User, Comment, blog, Like, Dislike
-# from .forms import CommentForm, blogForm, UpdateProfile
+from ..models import User, Comment, Blog, Like, Dislike
+from .forms import CommentForm, BlogForm, UpdateProfile
 from .. import db, photos
 
 @main.route('/')
@@ -58,7 +58,7 @@ def new_blog():
     '''
     A function that saves the blog added
     '''
-    blog_form = blogForm()
+    blog_form = BlogForm()
 
     if blog_form.validate_on_submit():
         title = blog_form.title.data
